63145791fb570b7c31a3f4f61d2fa985c1bb4da9
mustafa-krgoz <karagozhalitmustafa@gmail.com>
2025-03-20 18:20:18 +0300

    Projeye başlandı.Spring sürümleri yüklendi.

---
+++ src/main/java/com/Dental/Clinic/Website/DentalClinicWebsiteApplication.java
@@ -0,0 +1,13 @@
+package com.Dental.Clinic.Website;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DentalClinicWebsiteApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DentalClinicWebsiteApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/Dental/Clinic/Website/DentalClinicWebsiteApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Dental.Clinic.Website;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DentalClinicWebsiteApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
